72e5acb6621a1023dcbecc45baa18958bc2b6a00
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-04-08 15:37:33 -0400

    Added validations in case the user responds with incorrect input.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 // packages needed to run application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const {Circle, Triangle, Square} = require('./lib/shapes.js')
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
+const {colorhexadec, colorname} = require ('color')
 
 // questions for the user to be answered
 const questions = [
     {
         type: 'input',
         message: 'Choose characters for your logo. Select up to 3 characters.',
-        name: 'textchar'
+        name: 'textchar',
+        validate: (response) => {
+            if (response.textchar.length > 0 && response.textchar.length < 4 ) {
+                return true;
+            }
+            else return 'Invalid input! Please enter 1-3 characters.';
+        }
     },
 
     {
         type: 'input',
         message: 'Choose a text color for your logo.',
-        name: 'textcolor'
+        name: 'textcolor',
+        validate: (response) => {
+            if (colorhexadec(response) == true && colorname == true) {
+                return true;
+            }
+            else return 'Invvalid input! Please enter a valid color name or hexadecimal value.';
+        }
     },
 
     {
@@ -27,6 +40,12 @@ const questions = [
     {
         type: 'input',
         message: 'Choose a shape color for your logo.',
-        name: 'shapecolor'
+        name: 'shapecolor',
+        validate: (response) => {
+            if (colorhexadec(response) == true && colorname == true) {
+                return true;
+            }
+            else return 'Invvalid input! Please enter a valid color name or hexadecimal value.';
+        }
     }
 ]
