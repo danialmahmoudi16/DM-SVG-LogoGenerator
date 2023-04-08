aaea28d544a6b5cc27122a663b489abd6497ed43
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-04-08 18:11:55 -0400

    Added a writeTofile function, init function, and a function to generate the logo.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const inquirer = require('inquirer');
 const {Circle, Triangle, Square} = require('./lib/shapes.js');
 const {colorhexadec, colorname} = require ('color')
 
+// Function to write the logo.svg file 
+function writeTofile(fileName, data) {
+    fs.writeFile(fileName, data,
+        (err) => err ? console.log(err) : console.log("Success! logo.svg has been generated!") )
+}
+
 // questions for the user to be answered
 const questions = [
     {
@@ -48,4 +54,38 @@ const questions = [
             else return 'Invvalid input! Please enter a valid color name or hexadecimal value.';
         }
     }
-]
+];
+
+// Function that contains a switch case statement to determine which shape the user has selected
+function generateLogo(textchar, textcolor, shape, shapecolor) {
+    switch(shape){
+        case 'Circle':
+            const circle = new Circle(textchar, textcolor, shapecolor);
+            return circle.svgFile();
+        case 'Triangle':
+            const traingle = new Triangle(textchar, textcolor, shapecolor);
+            return triangle.svgFile();
+        case 'Square':
+            const square = new Square(textchar, textcolor, shapecolor);
+            return square.svgFile();
+    }
+}
+
+// Function that utilizes the user prompts to then intialize the logo
+function init() {
+    inquirer
+    .createPromptModule(questions)
+    .then((input)=>{
+        textcharacter = input.textchar
+        textcolor = input.textcolor
+        shape = input.shape
+        shapecolor = input.shapecolor
+        return generateLogo(input)
+    }
+    )
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+init();
